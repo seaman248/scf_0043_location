03c287b0112a88d4f5857709fb4cc99e0604d32e
seaman248 <semen.bko@ya.ru>
2017-03-21 20:14:25 +0700

    clean un_table from X and UNKN gambiae chrs

--- 2_orths_table_create.R
+++ 2_orths_table_create.R
@@ -15,4 +15,5 @@ scf_00043_gam_genes <- gam_genes[match(scf_00043_gam_genes, gam_genes$ensembl_ge
 
 un_table <- na.omit(bind_cols(scf_00043_genes, scf_00043_gam_genes))
 
+un_table <- un_table[un_table[, 7] != 'UNKN' & un_table[, 7] != 'X', ]
 write.csv2(un_table, './input_data/un_table.csv')
