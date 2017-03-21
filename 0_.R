2f5876ef5e6280bbef10e96da94206deb2c3d0de
seaman248 <semen.bko@ya.ru>
2017-03-21 18:53:33 +0700

    load all data

---
+++ 0_.R
@@ -0,0 +1,29 @@
+# load the data
+library(biomaRt)
+sp_of_interest <- c('astephensi_eg_gene', 'agambiae_eg_gene')
+HOST <- 'biomart.vectorbase.org'
+BASE <- listMarts(host=HOST)$biomart[1]
+
+orths <- getBM(attributes=c('ensembl_gene_id', 'agambiae_eg_gene'), mart = useMart(
+  BASE, host=HOST, dataset = 'astephensi_eg_gene'
+))
+
+orths <- orths[orths$agambiae_eg_gene != '',]
+
+geneSets <- lapply(sp_of_interest, function(DB_NAME){
+  getBM(
+    attributes=c('ensembl_gene_id','chromosome_name','start_position','end_position','strand'),
+    mart = useMart(
+      BASE,
+      host=HOST,
+      dataset = DB_NAME
+    )
+  )
+})
+names(geneSets) <- sp_of_interest
+
+lapply(sp_of_interest, function(sp){
+  write.csv2(geneSets[[sp]], paste0('./input_data/', sp, '_genes.csv'))
+})
+
+write.csv2(orths, './input_data/orths.csv')
