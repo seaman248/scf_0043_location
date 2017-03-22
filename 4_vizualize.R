2ca9561946cb255bb83b525ec78c2dbdcc2f3a35
seaman248 <semen.bko@ya.ru>
2017-03-22 09:57:42 +0700

    remove make_comparisons function

--- 4_vizualize.R
+++ 4_vizualize.R
@@ -25,19 +25,6 @@ xlims <- lapply(blocks, function(block){
   }))
 })
 
-make_comparisons <- function(segs){
-  comparisons <- list()
-  for(i in 1:(length(segs)-1)){
-    comparison_i <- comparison(data.frame(
-      start1=segs[[i]][,2], end1=segs[[i]][,3],
-      start2=segs[[(i+1)]][,2], end2=segs[[(i+1)]][,3]
-    ))
-    # comparison_i$direction=segs[[i]][,4]*segs[[(i+1)]][,4]
-    # #comparison_i$col <- unlist(lapply(comparison_i$direction, genCol))
-    comparisons[[i]] <- comparison_i
-  }
-  return(comparisons)
-}
 comparisons <- lapply(1:(length(dna_segs)-1), function(n){
   comparison(data.frame(
     start1=dna_segs[[n]][,2], end1=dna_segs[[n]][,3],
